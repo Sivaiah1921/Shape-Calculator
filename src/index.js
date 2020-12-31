6b72b8f83b8047b1e6518a049d1c646544421ce7
shankar <shankarreddy2025@gmail.com>
2020-12-31 10:25:44 +0530

    feat:covertedThisIntoFactoryMethod

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-
 let selectedShape;
-let areaResult;
 
 const  setRadioValue =()=> {
     let selectedShape = document.getElementsByName('area');
@@ -14,103 +12,89 @@ const  setRadioValue =()=> {
     }
 }
 
-class Shape {
-    constructor() {
-    }
-    area() {
-        return null;
-    }
-}
-//calculate the rectangle
-class Rectangle extends Shape {
-    constructor(l, w) {
-        super();
-        this.length = l
-        this.width = w
-    }
-    area() {
+//calculate the All areas to this factory Method
+var shapeFactory = function (...args){
+    // console.log(args)
+        var shape = {}
+        shape.length = args[0];
+        shape.width = args[1];
+        shape.diameter = args[0];
+        shape.side = args[0];
+        shape.aAxis = args[0];
+        shape.bAxis = args[1];
+
+    shape.printRecArea = function(){
         return this.length * this.width;
     }
-}
-//calculate the circle
-class Circle extends Shape {
-    constructor(diameter) {
-        super();
-        this.diameter = diameter
-    }
-    area() {
+    shape.printCircleArea = function(){
         return Math.PI * this.diameter;
     }
-}
-//calculate the squre
-class Square extends Shape {
-    constructor(side) {
-        super();
-        this.side = side
-    }
-    area() {
+    shape.printsquareArea = function(){
         return this.side * this.side;
     }
+    shape.printEllipseArea = function(){
+        return  Math.PI * this.aAxis * this.bAxis;
+    }
+ return shape
 }
-//calculate the elipse
-class Ellipse extends Shape {
-    constructor(aAxis, bAxis) {
-        super();
-        this.aAxis = aAxis
-        this.bAxis = bAxis
-    }
-    area() {
-        return Math.PI * this.aAxis * this.bAxis;
-    }
+// render the Rectangle function
+ function renderRec(){
+    let height = document.getElementById('rect-value-1').value;
+    let width = document.getElementById('rect-value-2').value;
+    let areaOfRec = shapeFactory(height, width)
+    let value = areaOfRec.printRecArea()
+    console.log(height,width,value)
+    document.getElementById("step-2").style.display = "none";
+    document.getElementById("step-3").style.display = "block";
+    document.getElementById("result-para").innerHTML = `You have calculated the area of a <b>rectangle</b> with height of ${height} and width of ${width}. Below is your result`;
+    document.getElementById("result-heading").innerHTML = `The area is ${value}`
+}
+// render the Cilrcle  function
+function rederCircle(){
+    let diameter = document.getElementById('circle-value').value;
+    let areaOfCircle = shapeFactory(diameter)
+    let areaResult = areaOfCircle.printCircleArea()
+    console.log(areaOfCircle.printCircleArea())
+    document.getElementById("step-2").style.display = "none";
+    document.getElementById("step-3").style.display = "block";
+    document.getElementById("result-para").innerHTML = `You have calculated the area of a <b>circle</b> with diameter of ${diameter}. Below is your result`;
+    document.getElementById("result-heading").innerHTML = `The area is ${areaResult}`;
+}
+// render the Square  function
+function renderSquare(){
+    let side = document.getElementById('square-value').value;
+    let areaOfSquare = shapeFactory(side)
+    let areaResult = areaOfSquare.printsquareArea()
+    document.getElementById("step-2").style.display = "none";
+    document.getElementById("step-3").style.display = "block";
+    document.getElementById("result-para").innerHTML = `You have calculated the area of a <b>square</b> with side of ${side}. Below is your result`;
+    document.getElementById("result-heading").innerHTML = `The area is ${areaResult}`;
+}
+// render the Ellipse  function
+function renderEllipse() {
+    let aAxis = document.getElementById('ellipse-value-1').value;
+    let bAxis = document.getElementById('ellipse-value-2').value;
+    let areaOfEllipse =shapeFactory(aAxis, bAxis)
+    let areaResult = areaOfEllipse.printEllipseArea()
+    console.log(aAxis,bAxis,areaResult)
+    document.getElementById("step-2").style.display = "none";
+    document.getElementById("step-3").style.display = "block";
+    document.getElementById("result-para").innerHTML = `You have calculated the area of a <b>ellipse</b> with A-axis of ${aAxis} and B-axis of ${bAxis}. Below is your result`;
+    document.getElementById("result-heading").innerHTML = `The area is ${areaResult}`;
 }
 
 //calculator logics.
 function onApplyValue() {
     switch (this.selectedShape) {
-        case "rectangle": {
-            let height = document.getElementById('rect-value-1').value;
-            let width = document.getElementById('rect-value-2').value;
-            let Area = new Rectangle(height, width)
-            this.areaResult = Area.area();
-            const value = Area.area();
-            document.getElementById("step-2").style.display = "none";
-            document.getElementById("step-3").style.display = "block";
-            document.getElementById("result-para").innerHTML = `You have calculated the area of a <b>rectangle</b> with height of ${height} and width of ${width}. Below is your result`;
-            document.getElementById("result-heading").innerHTML = `The area is ${value}`;
-            break;
-
-        }
-        case "circle": {
-            let diameter = document.getElementById('circle-value').value;
-            let Area = new Circle(diameter)
-            this.areaResult = Area.area()
-            document.getElementById("step-2").style.display = "none";
-            document.getElementById("step-3").style.display = "block";
-            document.getElementById("result-para").innerHTML = `You have calculated the area of a <b>circle</b> with diameter of ${diameter}. Below is your result`;
-            document.getElementById("result-heading").innerHTML = `The area is ${this.areaResult}`;
-            break;
-        }
-        case "square": {
-            let side = document.getElementById('square-value').value;
-            let Area = new Square(side)
-            this.areaResult = Area.area()
-            document.getElementById("step-2").style.display = "none";
-            document.getElementById("step-3").style.display = "block";
-            document.getElementById("result-para").innerHTML = `You have calculated the area of a <b>square</b> with side of ${side}. Below is your result`;
-            document.getElementById("result-heading").innerHTML = `The area is ${this.areaResult}`;
-            break;
-        }
-        case "ellipse": {
-            let aAxis = document.getElementById('ellipse-value-1').value;
-            let bAxis = document.getElementById('ellipse-value-2').value;
-            let Area = new Ellipse(aAxis, bAxis)
-            this.areaResult = Area.area()
-            document.getElementById("step-2").style.display = "none";
-            document.getElementById("step-3").style.display = "block";
-            document.getElementById("result-para").innerHTML = `You have calculated the area of a <b>ellipse</b> with A-axis of ${aAxis} and B-axis of ${bAxis}. Below is your result`;
-            document.getElementById("result-heading").innerHTML = `The area is ${this.areaResult}`;
-            break;
-        }
+        case "rectangle": renderRec()
+        break;
+        case "circle": rederCircle()
+        break;
+        case "square": renderSquare()
+        break;
+        case "ellipse": renderEllipse()
+        break;
+        default: return null
     }
 }
 // clear function 
@@ -124,5 +108,3 @@ function onClear() {
     document.getElementById("step-2").style.display = "none";
     document.getElementById("step-3").style.display = "none";
 }
-
-// console.log("first commit");
